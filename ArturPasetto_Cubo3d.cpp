3a6e9b0d665ff3a9a05eb225907ed5d829a4f1a9
ArturPasetto <56136030+ArturPasetto@users.noreply.github.com>
2020-06-12 10:38:54 -0300

    Update ArturPasetto_Cubo3d.cpp

--- ArturPasetto_Cubo3d.cpp
+++ ArturPasetto_Cubo3d.cpp
@@ -6,20 +6,20 @@
 
 
 void reshape(GLsizei largura, GLsizei altura) {
-	if (altura == 0) altura = 1; // previne a divis„o por zero
+	if (altura == 0) altura = 1; // previne a divis√£o por zero
 	GLfloat aspecto = (GLfloat)largura / (GLfloat)altura;
 	glViewport(0, 0, largura, altura);
-	glMatrixMode(GL_PROJECTION); // manipulando matriz de projeÁ„o
+	glMatrixMode(GL_PROJECTION); // manipulando matriz de proje√ß√£o
 	glLoadIdentity(); // zerando a matriz
 	gluPerspective(45, aspecto, 0.1, 100);
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // configura fundo sem transparencia
 	glEnable(GL_DEPTH_TEST); // alunos devem testar
-	glShadeModel(GL_SMOOTH); // acabamento com suavizaÁ„o
+	glShadeModel(GL_SMOOTH); // acabamento com suaviza√ß√£o
 	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST); // correcao de perspectiva
 }
 
 void keyboard(unsigned char tecla, int x, int y) {
-	
+
 	if (tecla == '\x1B') {
 		exit(EXIT_SUCCESS);
 	}
@@ -31,29 +31,29 @@ void keyboard(unsigned char tecla, int x, int y) {
 
 void piramide() {
 	glBegin(GL_TRIANGLES);
-		// triangulo 1 de 4 frente
-		glColor3f(1.0f, 0.0f, 0.0f); // vermelho troca em todos
-		glVertex3f(0.0f, 1.0f, 0.0f);
-		glVertex3f(-1.0f, -1.0f, 1.0f);
-		glVertex3f(1.0f, -1.0f, 1.0f);
+	// triangulo 1 de 4 frente
+	glColor3f(1.0f, 0.0f, 0.0f); // vermelho troca em todos
+	glVertex3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(-1.0f, -1.0f, 1.0f);
+	glVertex3f(1.0f, -1.0f, 1.0f);
 
-		// triangulo 2 de 4 direita
-		glColor3f(0.0f, 0.0f, 1.0f);
-		glVertex3f(0.0f, 1.0f, 0.0f);
-		glVertex3f(1.0f, -1.0f, 1.0f);
-		glVertex3f(1.0f, -1.0f, -1.0f);
+	// triangulo 2 de 4 direita
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(1.0f, -1.0f, 1.0f);
+	glVertex3f(1.0f, -1.0f, -1.0f);
 
-		// triangulo 3 de 4 atras
-		glColor3f(0.0f, 1.0f, 0.0f);
-		glVertex3f(0.0f, 1.0f, 0.0f);
-		glVertex3f(1.0f, -1.0f, -1.0f);
-		glVertex3f(-1.0f, -1.0f, -1.0f);
+	// triangulo 3 de 4 atras
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(1.0f, -1.0f, -1.0f);
+	glVertex3f(-1.0f, -1.0f, -1.0f);
 
-		// triangulo 4 de 4 esquerda
-		glColor3f(1.0f, 1.0f, 0.0f);
-		glVertex3f(0.0f, 1.0f, 0.0f);
-		glVertex3f(-1.0f, -1.0f, -1.0f);
-		glVertex3f(-1.0f, -1.0f, 1.0f);
+	// triangulo 4 de 4 esquerda
+	glColor3f(1.0f, 1.0f, 0.0f);
+	glVertex3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(-1.0f, -1.0f, -1.0f);
+	glVertex3f(-1.0f, -1.0f, 1.0f);
 
 	glEnd();
 }
@@ -61,49 +61,50 @@ void piramide() {
 void cubo() {
 	glBegin(GL_QUADS);
 
-		
-		//frente
-		glColor3f(1.0f, 1.0f, 0.0f);
-		glVertex3f(0.0f, 0.0f, -1.0f);
-		glVertex3f(0.0f, 1.0f, -1.0f);
-		glVertex3f(1.0f, 1.0f, 0.0f);
-		glVertex3f(1.0f, 0.0f, 0.0f);
 
-		//direita
-		glColor3f(0.0f, 1.0f, 0.0f);
-		glVertex3f(1.0f, 1.0f, 0.0f);
-		glVertex3f(1.0f, 0.0f, 0.0f);
-		glVertex3f(2.0f, 0.0f, -1.0f);
-		glVertex3f(2.0f, 1.0f, -1.0f);
-		
+	//frente
+	
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(0.0f, 0.0f, -1.0f);
+	glVertex3f(0.0f, 1.0f, -1.0f);
+	glVertex3f(1.0f, 1.0f, 0.0f);
+	glVertex3f(1.0f, 0.0f, 0.0f);
 
-		//atras
-		glColor3f(0.0f, 0.0f, 1.0f);
-		glVertex3f(2.0f, 0.0f, -1.0f);
-		glVertex3f(2.0f, 1.0f, -1.0f);
-		glVertex3f(1.0f, 1.0f, -2.0f);
-		glVertex3f(1.0f, 0.0f, -2.0f);
+	//direita
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(1.0f, 1.0f, 0.0f);
+	glVertex3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(2.0f, 0.0f, -1.0f);
+	glVertex3f(2.0f, 1.0f, -1.0f);
 
-		//esquerda
-		glColor3f(1.0f, 0.0f, 0.0f);
-		glVertex3f(1.0f, 1.0f, -2.0f);
-		glVertex3f(1.0f, 0.0f, -2.0f);
-		glVertex3f(0.0f, 0.0f, -1.0f);
-		glVertex3f(0.0f, 1.0f, -1.0f);
-		
-		//baixo
-		glColor3f(1.0f, 1.0f, 1.0f);
-		glVertex3f(0.0f, 0.0f, -1.0f);
-		glVertex3f(1.0f, 0.0f, -2.0f);
-		glVertex3f(2.0f, 0.0f, -1.0f);
-		glVertex3f(1.0f, 0.0f, 0.0f);
 
-		//cima
-		glColor3f(0.5f, 0.5f, 0.5f);
-		glVertex3f(1.0f, 1.0f, 0.0f);
-		glVertex3f(0.0f, 1.0f, -1.0f);
-		glVertex3f(1.0f, 1.0f, -2.0f);
-		glVertex3f(2.0f, 1.0f, -1.0f);
+	//atras
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(2.0f, 0.0f, -1.0f);
+	glVertex3f(2.0f, 1.0f, -1.0f);
+	glVertex3f(1.0f, 1.0f, -2.0f);
+	glVertex3f(1.0f, 0.0f, -2.0f);
+
+	//esquerda
+	glColor3f(0.5f, 0.5f, 0.5f);
+	glVertex3f(1.0f, 1.0f, -2.0f);
+	glVertex3f(1.0f, 0.0f, -2.0f);
+	glVertex3f(0.0f, 0.0f, -1.0f);
+	glVertex3f(0.0f, 1.0f, -1.0f);
+
+	//baixo
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex3f(0.0f, 0.0f, -1.0f);
+	glVertex3f(1.0f, 0.0f, -2.0f);
+	glVertex3f(2.0f, 0.0f, -1.0f);
+	glVertex3f(1.0f, 0.0f, 0.0f);
+
+	//cima
+	glColor3f(1.0f, 1.0f, 0.0f);
+	glVertex3f(1.0f, 1.0f, 0.0f);
+	glVertex3f(0.0f, 1.0f, -1.0f);
+	glVertex3f(1.0f, 1.0f, -2.0f);
+	glVertex3f(2.0f, 1.0f, -1.0f);
 
 	glEnd();
 
@@ -117,23 +118,24 @@ void display() {
 	// desenho da piramide 3d
 	glLoadIdentity();
 
-	
-	glPushMatrix();
-		glTranslatef(-1.5f, 0.0f, -6.0f);
-		piramide();
-	glPopMatrix();
-	
 
 	glPushMatrix();
-		glTranslatef(0.0f, 0.0f, -5.0f);
-		cubo();
+	glTranslatef(-1.5f, 0.0f, -6.0f);
+	piramide();
+	glPopMatrix();
+
+
+	glPushMatrix();
+	glTranslatef(0.0f, 0.0f, -5.0f);
+	glRotatef(30, 1, 0 , 0);
+	cubo();
 	glPopMatrix();
 
 	glutSwapBuffers(); // trocar a matriz da tela por esta aqui.
 }
 
 
-int main(int argc, char** argv){
+int main(int argc, char** argv) {
 
 	glutInit(&argc, argv); // Ligar GLUT
 	glutInitDisplayMode(GLUT_DOUBLE); // Display com Duplo Buffer
